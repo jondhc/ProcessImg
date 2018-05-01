637b9d8447359ffbe101a54bc474b01adadb1a93
Julio <julioespinosa@outlook.com>
2018-05-01 13:24:24 -0500

    inverted

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import numpy
 import cv2
 from matplotlib import pyplot
+import numpy as np;
 
+im_in = cv2.imread("animal.jpg", cv2.IMREAD_GRAYSCALE);
+th, im_th = cv2.threshold(im_in, 220, 255, cv2.THRESH_BINARY_INV);
+cv2.imshow("Thresholded Image", im_th)
+cv2.waitKey(0)
 
 image = cv2.imread('animal.jpg')
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -10,9 +15,7 @@ cv2.imshow('color_image',image)
 cv2.imshow('gray_image',gray_image)
 cv2.waitKey(0)                 # Waits forever for user to press any key
 cv2.destroyAllWindows()
-new_img = Image.open('kids.jpg')
-grayscale(new_img)
-new_img.show()
+
 
 bgrKids = cv2.imread('kids.jpg', -1) # Read image [0], [1] flag -1 is used to load it unchanged. Loaded in BGR.
 bgrAnimal = cv2.imread('animal.jpg', -1)
