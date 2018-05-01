e7dd5b9e03d56a3b763cd6ec2f350b3b8c024f29
AlbertoElTuerto <nitsugaotsuga@gmail.com>
2018-05-01 16:18:26 -0500

    With Threshold Binary, Threshold Binary Inverted, Contrast Stretch, POSSIBLE Inverse Operator and POSSIBLE Inverted Gray Scale Threshold Operator.

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ bgrPortrait = cv2.imread('portrait.jpg', -1)
 bgrReef = cv2.imread('reef.jpg', -1)
 bgrScenery = cv2.imread('scenery.jpg', -1)
 
-bgrImage = bgrScenery
+bgrImage = bgrPortrait
 
 b,g,r = cv2.split(bgrImage) # Split bgr image
 rgbImage = cv2.merge([r,g,b]) # Merge using rgb order
@@ -25,7 +25,6 @@ cv2.destroyAllWindows() # Destroy created windows
 cv2.imwrite('result.jpg', bgrImage) # Save [1] image in the working directory with specified name [0]
 
 def thresholdBinary (img):
-
     # Pasar una imagen a escala de grises
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -46,6 +45,7 @@ def thresholdBinaryInv(img):
     return thresholdBinInv
 
 def contrastStretch(img):
+    # Pasar una imagen a escala de grises
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     #dimensiones de la imagen
@@ -65,6 +65,25 @@ def contrastStretch(img):
     #regresar la imagen
     return gray_img
 
+# POSSIBLY the Inverted Gray Scale Threshold Operator
+def grayInverse(img):
+    # Pasar una imagen a escala de grises
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Invert gray scale image
+    invertedImg = cv2.bitwise_not(gray_img)
+
+    #regresar la imagen
+    return invertedImg
+
+# POSSIBLY the Inverse Operator
+def colorInverse(img):
+    b, g, r = cv2.split(img)  # Split bgr image
+    b = 255 - b # invert b component
+    g = 255 - g # invert g component
+    r = 255 - r # invert r component
+    bgrImgInverted = cv2.merge([b, g, r])  # Merge using bgr order
+    return bgrImgInverted
 
 # original image in gray scale
 originalGray = cv2.cvtColor(bgrImage, cv2.COLOR_BGR2GRAY)
@@ -74,16 +93,25 @@ res1 = contrastStretch(bgrImage)
 res2 = thresholdBinary(bgrImage)
 # original with threshold binary inverted
 res3 = thresholdBinaryInv(bgrImage)
-
+# original with gray scale inverted
+res4 = grayInverse(bgrImage)
+# original with inverted colors
+res5 = colorInverse(bgrImage)
 
 cv2.namedWindow('thresholdBinary', cv2.WINDOW_NORMAL)
 cv2.imshow('thresholdBinary', res2)
 cv2.namedWindow('thresholdBinaryInverted', cv2.WINDOW_NORMAL)
 cv2.imshow('thresholdBinaryInverted', res3)
-cv2.namedWindow('original', cv2.WINDOW_NORMAL) # Set new window [0], with flags [1]
-cv2.imshow('original', originalGray) # Show [1] image in a [0] window
+cv2.namedWindow('originalGray', cv2.WINDOW_NORMAL) # Set new window [0], with flags [1]
+cv2.imshow('originalGray', originalGray) # Show [1] image in a [0] window
 cv2.namedWindow('contrastStretch', cv2.WINDOW_NORMAL) # Set new window [0], with flags [1]
 cv2.imshow('contrastStretch', res1) # Show [1] image in a [0] window
+cv2.namedWindow('grayScaleInverted', cv2.WINDOW_NORMAL)
+cv2.imshow('grayScaleInverted', res4)
+cv2.namedWindow('colorInverted', cv2.WINDOW_NORMAL)
+cv2.imshow('colorInverted', res5)
+cv2.namedWindow('ORIGINAL', cv2.WINDOW_NORMAL)
+cv2.imshow('ORIGINAL', bgrImage)
 cv2.waitKey(0) # Wait [0] miliseconds for a keyboard event for the program to continue
 cv2.destroyAllWindows() # Destroy created windows
 
