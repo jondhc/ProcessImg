ff6358d33965000fd96abf9b0281d573f2e3926c
luisricardo <luis.montesgz@udlap.mx>
2018-05-01 21:02:20 -0500

    Eyes Detection

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
+from datetime import datetime
 
 def imageAdition(img1, img2):
     img3 = cv2.add(img1,img2)
@@ -20,6 +21,22 @@ def imageRotaton(img, degrees):
     M = cv2.getRotationMatrix2D((cols / 2, rows / 2), degrees, 1)
     dst = cv2.warpAffine(img, M, (cols, rows))
     return dst
+
+def circleDetecion(img):
+    src = cv2.medianBlur(img,5)
+    src = cv2.cvtColor(src,cv2.COLOR_BGR2GRAY)
+
+    circles = cv2.HoughCircles(src, cv2.HOUGH_GRADIENT, 1, 20 , param1=50,param2=30,
+                               minRadius=0,maxRadius=0)
+    circles = np.uint16(np.around(circles))
+    for i in circles[0,:]:
+        cv2.circle(img,(i[0], i[1]), i[2], (0,255,0), 2)
+        cv2.circle(img, (i[0], i[1]), 2, (0, 0, 255), 3)
+    cv2.imshow('detected circles', img)
+    return img
+
+
+
 #------------------------------------Load Images------------------------------------
 #------------------------------------Load Images------------------------------------
 #------------------------------------Load Images------------------------------------
@@ -31,14 +48,13 @@ bgrLandscape = cv2.imread('landscape.jpg', -1)
 bgrNature = cv2.imread('nature.jpg', -1)
 bgrPortrait = cv2.imread('portrait.jpg', -1)
 bgrReef = cv2.imread('reef.jpg',0)
-bgrJaja = cv2.imread('jaja.jpg',0)
+bgrOjo = cv2.imread('ojo.jpg',-1)
 
 res = imageAdition(bgrAnimal, bgrCity)
 res2 = imageSubstraction(bgrAnimal, bgrCity)
 
 res3 = imageConvolution(bgrAnimal)
 
-res4 = imageRotaton(bgrJaja, 75)
 
 cv2.namedWindow('image', cv2.WINDOW_NORMAL) # Set new window [0], with flags [1]
 cv2.imshow('image', res) # Show [1] image in a [0] window
@@ -48,8 +64,18 @@ cv2.namedWindow('convolution', cv2.WINDOW_NORMAL)
 cv2.imshow('convolution', res3)
 cv2.namedWindow('original', cv2.WINDOW_NORMAL)
 cv2.imshow('original', bgrAnimal)
+
+instanteInicial = datetime.now()
+print("Iniciando")
+
+res4 = circleDetecion(bgrOjo)
 cv2.namedWindow('rotation', cv2.WINDOW_NORMAL)
 cv2.imshow('rotation', res4)
+
+instanteFinal = datetime.now()
+time = instanteFinal - instanteInicial
+print(time.seconds)
+
 cv2.waitKey(0) # Wait [0] miliseconds for a keyboard event for the program to continue
 cv2.destroyAllWindows() # Destroy created windows
 
